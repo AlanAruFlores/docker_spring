47b8699994109c073e2aae5bbf99bd8b65d901ff
AlanAruFlores <alanaruquipa242@gmail.com>
2025-04-06 14:00:28 -0300

    docker example

--- Spring-Docker-init/src/main/java/com/example/SpringDockerApplication.java
+++ Spring-Docker-init/src/main/java/com/example/SpringDockerApplication.java
@@ -1,7 +1,13 @@
 package com.example;
 
+import com.example.entity.User;
+import com.example.repository.IUserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class SpringDockerApplication {
@@ -10,4 +16,17 @@ public class SpringDockerApplication {
 		SpringApplication.run(SpringDockerApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner runner(IUserRepository userRepository){
+		return (args -> {
+			List<User> users = List.of(
+					new User(1L,"Alan", "Aruquipa"),
+					new User(2L,"Ale", "Flores"),
+					new User(3L,"Marcos", "Bordon"),
+					new User(4L,"Karl", "Montoya")
+					);
+
+            userRepository.saveAll(users);
+		});
+	}
 }
